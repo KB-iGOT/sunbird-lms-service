96b791180d2d8067054098a2a870ab8a9a3b64db
ruksana2808 <111083125+ruksana2808@users.noreply.github.com>
2024-08-20 11:30:15 +0530

    Added the code for v2 GET API settings/get/cadreConfig (#5)
    * Added the code for v2 GET API settings/get/cadreConfig
    
    * Made changes according to the review
    
    * Made changes according to the review

--- service/src/main/java/org/sunbird/actor/systemsettings/SystemSettingsActor.java
+++ service/src/main/java/org/sunbird/actor/systemsettings/SystemSettingsActor.java
@@ -1,7 +1,18 @@
 package org.sunbird.actor.systemsettings;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import java.text.MessageFormat;
 import java.util.Map;
+import org.apache.commons.lang3.ObjectUtils;
+import org.apache.http.HttpStatus;
+import org.apache.http.protocol.HTTP;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.sunbird.actor.core.BaseActor;
+import org.sunbird.exception.ProjectCommonException;
+import org.sunbird.exception.ResponseCode;
+import org.sunbird.exception.ResponseMessage;
+import org.sunbird.exception.ResponseMessage.Message;
 import org.sunbird.keys.JsonKey;
 import org.sunbird.model.systemsettings.SystemSetting;
 import org.sunbird.request.Request;
@@ -11,6 +22,7 @@ import org.sunbird.service.systemsettings.SystemSettingsService;
 
 public class SystemSettingsActor extends BaseActor {
 
+  private static final Logger log = LoggerFactory.getLogger(SystemSettingsActor.class);
   private final SystemSettingsService service = new SystemSettingsService();
 
   @Override
@@ -24,6 +36,9 @@ public class SystemSettingsActor extends BaseActor {
         break;
       case "setSystemSetting":
         setSystemSetting(request);
+        break;
+      case "getSystemSettingV2":
+        getSystemSettingV2(request);
         break;
       default:
         onReceiveUnsupportedOperation();
@@ -51,5 +66,28 @@ public class SystemSettingsActor extends BaseActor {
     Map<String, Object> request = actorMessage.getRequest();
     Response response = service.setSystemSettings(request, actorMessage.getRequestContext());
     sender().tell(response, self());
+  }
+
+  private void getSystemSettingV2(Request actorMessage) {
+    String key = (String) actorMessage.getContext().get(JsonKey.FIELD);
+    JsonNode setting =
+        service.getSystemSettingV2ByKey(
+            key,
+            actorMessage.getRequestContext());
+    Response response = new Response();
+    if (setting != null && !setting.isNull()) {
+      response.put(JsonKey.RESPONSE, setting);
+      sender().tell(response, self());
+    } else {
+      String formattedErrorMessage = MessageFormat.format(
+          ResponseCode.resourceNotFound.getErrorMessage(), key);
+      ProjectCommonException exception = new ProjectCommonException(
+          ResponseCode.resourceNotFound,
+          formattedErrorMessage,
+          ResponseCode.RESOURCE_NOT_FOUND.getResponseCode()
+      );
+      sender().tell(exception, self());
+    }
+
   }
 }
