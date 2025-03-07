f7df565635152b1bc8d7785d498279e6a0b1f4fc
anilkumarkammalapalli <121931293+anilkumarkammalapalli@users.noreply.github.com>
2025-03-07 12:20:42 +0530

    4.8.21 user es update (#35)
    * Learner service changes - user creation - KB-8219 (#28)
    
    * Learner service changes - user creation
    
    * code refactor
    
    * Review Changes
    
    * Removing white spaces and comments
    
    * Using Constants for variables
    
    * Code Optimisation
    
    * profileDetails creation and refactored data handling
    
    * worked on code review comments
    
    * Ensured personalDetails, professionalDetails, and additionalProperties are only added if non-empty
    
    ---------
    
    Co-authored-by: sreerag-tarento <sreerag.sajeesh@tarento.com>

--- service/src/main/java/org/sunbird/actor/user/SSOUserCreateActor.java
+++ service/src/main/java/org/sunbird/actor/user/SSOUserCreateActor.java
@@ -1,12 +1,14 @@
 package org.sunbird.actor.user;
 
 import akka.actor.ActorRef;
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import java.text.MessageFormat;
 import java.util.*;
 import javax.inject.Inject;
 import javax.inject.Named;
 import org.apache.commons.collections.CollectionUtils;
+import org.apache.commons.collections.MapUtils;
 import org.apache.commons.lang3.StringUtils;
 import org.sunbird.actor.user.validator.UserRequestValidator;
 import org.sunbird.common.ElasticSearchHelper;
@@ -83,7 +85,7 @@ public class SSOUserCreateActor extends UserBaseActor {
    * @param actorMessage Request
    */
 
-  private void createUserV5(Request actorMessage) {
+  private void createUserV5(Request actorMessage) throws JsonProcessingException {
     logger.debug(actorMessage.getRequestContext(), "SSOUserCreateActor:createV5User: starts : ");
     populateRoles(actorMessage, findRootOrgId(actorMessage));
     createBasisProfileDetails(actorMessage);
@@ -269,7 +271,7 @@ public class SSOUserCreateActor extends UserBaseActor {
   }
 
   private void populateRoles(Request actorMessage, String rootOrgId ) {
-    Map<String, Object> userMap = actorMessage.getRequest();
+    Map<String, Object> userMap = (Map<String, Object>) actorMessage.getRequest();
     if (userMap.get(JsonKey.ROLES) == null || ((List<String>) userMap.get(JsonKey.ROLES)).isEmpty()) {
       userMap.put(JsonKey.ROLES, Arrays.asList(JsonKey.PUBLIC));
     } else {
@@ -277,15 +279,61 @@ public class SSOUserCreateActor extends UserBaseActor {
     }
   }
 
-  private void createBasisProfileDetails(Request actorMessage) {
+  private void createBasisProfileDetails(Request actorMessage) throws JsonProcessingException {
     Map<String, Object> userMap = actorMessage.getRequest();
     Map<String, Object> profileDetails = new HashMap<>();
+    Map<String, Object> employmentDetails = Map.of(JsonKey.DEPARTMENT_NAME, userMap.getOrDefault(JsonKey.CHANNEL, ""));
+    Map<String, Object> additionalProperties = new HashMap<>();
+    List<Map<String, Object>> professionalDetailsList = new ArrayList<>();
+    Map<String, Object> professionalDetails = new HashMap<>();
+    Map<String, Object> personalDetails = new HashMap<>();
+
+    Map<String, Object> personalDetailsRequest = (Map<String, Object>) userMap.getOrDefault(JsonKey.PERSONAL_DETAILS, Map.of());
+    if (!personalDetailsRequest.isEmpty()) {
+      personalDetailsRequest.forEach((key, value) -> addIfNotEmpty(personalDetails, key, value));
+
+      Object tags = personalDetails.remove(JsonKey.TAGS);
+      if (tags instanceof List && !((List<?>) tags).isEmpty()) {
+        additionalProperties.put(JsonKey.TAGS, tags);
+      }
+
+      addIfNotEmpty(professionalDetails, JsonKey.DESIGNATION, personalDetails.remove(JsonKey.DESIGNATION));
+      addIfNotEmpty(professionalDetails, JsonKey.GROUP, personalDetails.remove(JsonKey.GROUP));
+    }
+
+    if (!professionalDetails.isEmpty()) {
+      professionalDetailsList.add(professionalDetails);
+      profileDetails.put(JsonKey.PROFESSIONAL_DETAILS, professionalDetailsList);
+    }
+
+    profileDetails.put(JsonKey.EMPLOYMENT_DETAILS, employmentDetails);
     profileDetails.put(JsonKey.PROFILE_GROUP_STATUS, "NOT-VERIFIED");
     profileDetails.put(JsonKey.PROFILE_DESIGNATION_STATUS, "NOT-VERIFIED");
     profileDetails.put(JsonKey.PROFILE_STATUS, "NOT-VERIFIED");
     profileDetails.put(JsonKey.MANDATORY_FIELDS_EXISTS, false);
-    userMap.put(JsonKey.PROFILE_DETAILS, profileDetails.toString());
+
+    if (!additionalProperties.isEmpty()) {
+      profileDetails.put(JsonKey.ADDITIONAL_PROPERTIES, additionalProperties);
+    }
+
+    if (!personalDetails.isEmpty()) {
+      profileDetails.put(JsonKey.PERSONAL_DETAILS, personalDetails);
+    }
+    userMap.put(JsonKey.PROFILE_DETAILS, mapper.writeValueAsString(profileDetails));
   }
+
+  private void addIfNotEmpty(Map<String, Object> map, String key, Object value) {
+    if (value instanceof String && !((String) value).isBlank()) {
+      map.put(key, value);
+    } else if (value instanceof List && !((List<?>) value).isEmpty()) {
+      map.put(key, value);
+    } else if (value instanceof Map && !((Map<?, ?>) value).isEmpty()) {
+      map.put(key, value);
+    } else if (value != null) {
+      map.put(key, value);
+    }
+  }
+
 
   private void checkIfMDOLeaderExist(Map<String,Object> userMap, Request actorMessage, String rootOrgId){
     List<String> roles = (List<String>) userMap.get(JsonKey.ROLES);
