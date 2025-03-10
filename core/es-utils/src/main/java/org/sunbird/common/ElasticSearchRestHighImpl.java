e2eed06f477b190ef7819c1ea74d980fcac961c3
Sahil-tarento <140611066+Sahil-tarento@users.noreply.github.com>
2025-03-10 12:56:08 +0530

    4.8.21 merged (#47)
    * Cbrelease 4.8.20 (#30)
    
    * KB-7932 | At MDO Portal we are getting Error While Creating Individual User. (#24)
    
    1. Added filter with status 1.
    
    * Enhanced search to handle full email address (#25)
    
    * Using ES Update command instead of save
    
    ---------
    
    Co-authored-by: tarentomaheshvakkund <139739142+tarentomaheshvakkund@users.noreply.github.com>
    
    * Adding shaksham AI enable (#37)
    
    * Adding shaksham AI enable (#40)
    
    * Adding shaksham AI enable (#41)
    
    * Adding sakshamAIenabled
    
    * KB-8606 | changes for MDO_Leader unable to assign other roles (#45)
    
    * KB-8226 | changes for spv portal org partial search (#46)
    
    ---------
    
    Co-authored-by: Karthikeyan Rajendran <70887864+karthik-tarento@users.noreply.github.com>
    Co-authored-by: tarentomaheshvakkund <139739142+tarentomaheshvakkund@users.noreply.github.com>
    Co-authored-by: MukeshLingutla <168708131+MukeshLingutla@users.noreply.github.com>

--- core/es-utils/src/main/java/org/sunbird/common/ElasticSearchRestHighImpl.java
+++ core/es-utils/src/main/java/org/sunbird/common/ElasticSearchRestHighImpl.java
@@ -372,7 +372,8 @@ public class ElasticSearchRestHighImpl implements ElasticSearchService {
     if (!StringUtils.isBlank(searchDTO.getQuery())) {
       if (searchDTO.getQuery().contains("@")) {
         query.must(QueryBuilders.termQuery(JsonKey.PROFILE_PRIMARY_EMAIL_FIELD, searchDTO.getQuery()));
-      } else if (searchDTO.getQuery().contains(JsonKey.MATCH_PHRASE_PREFIX)) {
+      }
+      if (searchDTO.getQuery().contains(JsonKey.MATCH_PHRASE_PREFIX)) {
         try {
           JSONObject queryJson = new JSONObject(searchDTO.getQuery());  // Parse JSON query
           QueryBuilder matchPhrasePrefixQuery = QueryBuilders.wrapperQuery(queryJson.toString());
