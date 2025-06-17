7b686c70bf8f3d9345e59d693295adab7d26dc14
Sahil-tarento <140611066+Sahil-tarento@users.noreply.github.com>
2025-06-17 16:10:39 +0530

    KB-10186 | Learner Portal | If user has Primary details, in the Service history default data is not calculating for Profile page percentage. (#78) (#79)
    1.If user has Professional details and the Service history data is not there then we need to add the profile completion percentage based on the professional details
    
    Co-authored-by: tarentomaheshvakkund <139739142+tarentomaheshvakkund@users.noreply.github.com>

--- core/platform-common/src/main/java/org/sunbird/keys/JsonKey.java
+++ core/platform-common/src/main/java/org/sunbird/keys/JsonKey.java
@@ -701,5 +701,7 @@ public final class JsonKey {
   public static final String USER_EXTENDED_PROFILE_READ_FIELDS = "user_extended_profile_read_fields";
   public static final String PROFILE_COMPLETION_FIELD_WEIGHT="profile.completion.field.weight";
   public static final String PROFILE_COMPLETION_PERCENTAGE = "profileCompletionPercentage";
+  public static final String SERVICE_HISTORY = "serviceHistory";
+
   private JsonKey() {}
 }
