b0944a82f23db81d5673db088c560236740dde64
ruksanasemeir <ruksana.semeir@tarento.com>
2025-06-24 15:14:48 +0530

    Fix for KB-10188

--- core/es-utils/src/main/java/org/sunbird/common/ElasticSearchRestHighImpl.java
+++ core/es-utils/src/main/java/org/sunbird/common/ElasticSearchRestHighImpl.java
@@ -7,6 +7,7 @@ import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 import java.util.function.Predicate;
+import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 import org.apache.commons.collections.CollectionUtils;
 import org.apache.commons.collections.MapUtils;
@@ -372,7 +373,8 @@ public class ElasticSearchRestHighImpl implements ElasticSearchService {
     }
     // apply simple query string
     if (!StringUtils.isBlank(searchDTO.getQuery())) {
-      if (searchDTO.getQuery().contains("@")) {
+      //validate whether regex pattern  refer userRegisterImp in cb-ext
+      if (isValidEmail(searchDTO.getQuery())) {
         query.must(QueryBuilders.termQuery(JsonKey.PROFILE_PRIMARY_EMAIL_FIELD, searchDTO.getQuery()));
       } else {
         SimpleQueryStringBuilder sqsb = QueryBuilders.simpleQueryStringQuery(searchDTO.getQuery());
@@ -508,6 +510,13 @@ public class ElasticSearchRestHighImpl implements ElasticSearchService {
 
     ConnectionManager.getRestClient().searchAsync(searchRequest, listener);
     return promise.future();
+  }
+
+  private boolean isValidEmail(String email) {
+    String emailRegex = "^[a-zA-Z0-9_+&*-]+(?:\\.[a-zA-Z0-9_+&*-]+)*@" +
+            "(?:[a-zA-Z0-9-]+\\.)+[a-zA-Z]{2,7}$";
+    Pattern pat = Pattern.compile(emailRegex);
+    return email != null && pat.matcher(email).matches();
   }
 
   /**
