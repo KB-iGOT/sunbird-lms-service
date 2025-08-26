3524b104426b30c6bc5b1ef1c5fea88ad4c998d5
sreerag <sreerag.sajeesh@tarento.com>
2025-08-26 13:57:41 +0530

    KB-11342 - Phone Number Field Should Be Mandatory During User Creation

--- service/src/main/java/org/sunbird/actor/user/ManagedUserActor.java
+++ service/src/main/java/org/sunbird/actor/user/ManagedUserActor.java
@@ -1,5 +1,6 @@
 package org.sunbird.actor.user;
 
+import java.text.MessageFormat;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -12,6 +13,7 @@ import org.sunbird.actor.user.validator.UserCreateRequestValidator;
 import org.sunbird.common.ElasticSearchHelper;
 import org.sunbird.dto.SearchDTO;
 import org.sunbird.exception.ProjectCommonException;
+import org.sunbird.exception.ResponseCode;
 import org.sunbird.keys.JsonKey;
 import org.sunbird.model.location.Location;
 import org.sunbird.request.Request;
@@ -63,6 +65,7 @@ public class ManagedUserActor extends UserBaseActor {
   private void createManagedUser(Request actorMessage) {
     actorMessage.toLower();
     Map<String, Object> userMap = actorMessage.getRequest();
+    validateEmailAndPhone(userMap);
     populateLocationCodesFromProfileLocation(userMap);
     if (userMap.containsKey(JsonKey.ORG_EXTERNAL_ID)) {
       userMap.remove(JsonKey.ORG_EXTERNAL_ID);
@@ -235,4 +238,25 @@ public class ManagedUserActor extends UserBaseActor {
     response.put(JsonKey.RESPONSE, responseMap);
     sender().tell(response, self());
   }
+
+  private void validateEmailAndPhone(Map<String,Object> userMap) {
+    if (StringUtils.isBlank((String) userMap.get(JsonKey.PHONE))) {
+      ProjectCommonException.throwClientErrorException(
+              ResponseCode.mandatoryParamsMissing,
+              MessageFormat.format(ResponseCode.mandatoryParamsMissing.getErrorMessage(), JsonKey.PHONE)
+      );
+    }
+    if (StringUtils.isBlank((String) userMap.get(JsonKey.EMAIL))) {
+      ProjectCommonException.throwClientErrorException(
+              ResponseCode.mandatoryParamsMissing,
+              MessageFormat.format(ResponseCode.mandatoryParamsMissing.getErrorMessage(), JsonKey.EMAIL)
+      );
+    }
+    // check phone and uniqueness using user look table
+    userLookupService.checkPhoneUniqueness(
+            (String) userMap.get(JsonKey.PHONE), null);
+    userLookupService.checkEmailUniqueness(
+            (String) userMap.get(JsonKey.EMAIL), null);
+  }
+
 }
