279c0490ecc3010dcd23a81e5dbe327ab8e44704
Sahil-tarento <140611066+Sahil-tarento@users.noreply.github.com>
2025-10-10 10:55:58 +0530

    4.8.29 dev v1 (#104)
    * KB-11432:: stateOrMinistry update in user profile details (#102)
    
    * KB-11432:: stateOrMinistry update in user profile details
    
    * KB-11432:: stateOrMinistry update in user profile details
    
    * KB-11432:: stateOrMinistry update in user profile details
    
    * KB-11432:: stateOrMinistry update in user profile details (#103)
    
    * KB-11432:: stateOrMinistry update in user profile details
    
    * KB-11432:: stateOrMinistry update in user profile details
    
    * KB-11432:: stateOrMinistry update in user profile details
    
    * KB-11432:: stateOrMinistry update in user profile details
    
    ---------
    
    Co-authored-by: Arpitha <arpitha.sureshappa@tarento.com>

--- core/platform-common/src/main/java/org/sunbird/keys/JsonKey.java
+++ core/platform-common/src/main/java/org/sunbird/keys/JsonKey.java
@@ -707,6 +707,9 @@ public final class JsonKey {
   public static final String ORG_DEFAULT_ES_QUERY_FIELDS = "org_default_es_query_fields";
   public static final String USER_PROFILE_UPDATE_TOPIC = "kafka_user_profile_update_topic";
   public static final String CCA_ENABLED = "iscca";
+  public static final String MINISTRY_STATE_ID = "ministryOrStateId";
+  public static final String MINISTRY_STATE_ORG_NAME = "ministryOrStateOrgName";
+  public static final String SUPPORT_USER_CREATE = "supportUserCreate";
 
   private JsonKey() {}
 }
