281390af2a0bc4b83da524001c3d3328525a3543
Karthikeyan R <karthikeyanr@TI-MAC-118.local>
2026-02-03 12:31:46 +0530

    Added Keycloack 24 change

--- core/platform-common/src/main/java/org/sunbird/sso/KeycloakUtil.java
+++ core/platform-common/src/main/java/org/sunbird/sso/KeycloakUtil.java
@@ -42,7 +42,7 @@ public class KeycloakUtil {
 
   public static String getAdminAccessTokenWithoutDomain(RequestContext context) throws Exception {
     String url = ProjectUtil.getConfigValue(JsonKey.SUNBIRD_SSO_LB_IP)
-        + "/auth/realms/"
+        + "/realms/"
         + ProjectUtil.getConfigValue(JsonKey.SUNBIRD_SSO_RELAM)
         + "/protocol/openid-connect/token";
     return getAdminAccessToken(context, url);
