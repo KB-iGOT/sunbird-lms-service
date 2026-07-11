ee2e0e44be5355907a16744fa4b0377fa2ed9847
shankaragoudab <140387294+shankaragoudab@users.noreply.github.com>
2026-07-11 22:36:00 +0530

    KB-14082 : role assignment config update (#192)

--- core/platform-common/src/main/java/org/sunbird/keys/JsonKey.java
+++ core/platform-common/src/main/java/org/sunbird/keys/JsonKey.java
@@ -743,6 +743,7 @@ public final class JsonKey {
     public static final String ORG_DESCRIPTION_MAX_LENGTH = "org_description_max_length";
     public static final String PUBLIC_UPPER_CASE = "PUBLIC";
     public static final String LEADER_SUFFIX = "_LEADER";
+    public static final String ADMIN_ROLE_SUFFIXES = "admin_role_suffixes";
 
 
     private JsonKey() {
