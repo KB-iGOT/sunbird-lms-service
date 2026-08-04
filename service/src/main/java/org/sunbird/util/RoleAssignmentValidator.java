eca1f8afc40e19948eeff1d64973564f9b41622d
ramyaranganathan-1775 <ramya.ranganathan@tarento.com>
2026-08-04 22:14:31 +0530

    Cbrelease 4.8.39 (#248)
    * KB-14924 : igot support role changes (#242)
    
    * igot support role changes
    
    * update createdby field while user create
    
    * KB-15028 : state admin role assignment changes (#244)
    
    * igot support role changes
    
    * update createdby field while user create
    
    * KB-14082 : updated state admin role assignment
    
    ---------
    
    Co-authored-by: shankaragoudab <140387294+shankaragoudab@users.noreply.github.com>

--- service/src/main/java/org/sunbird/util/RoleAssignmentValidator.java
+++ service/src/main/java/org/sunbird/util/RoleAssignmentValidator.java
@@ -31,7 +31,11 @@ public class RoleAssignmentValidator {
   public void validateRoleAssignment(String requestingUserId, String requestingUserOrgId, String targetOrgId, String targetUserId, List<String> rolesToAssign, RequestContext context) {
     List<String> requestingUserRoles = validateRequestingUserRoles(requestingUserId, context);
     boolean isSpv = false;
-    if (requestingUserRoles.contains(JsonKey.SPV_ADMIN)) {
+    String configuredSpvRoles = ProjectUtil.getConfigValue(JsonKey.SPV_ROLES);
+    List<String> spvRoles = StringUtils.isNotBlank(configuredSpvRoles)
+            ? List.of(configuredSpvRoles.split(","))
+            : List.of(JsonKey.SPV_ADMIN, JsonKey.IGOT_SUPPORT_ADMIN);
+    if (requestingUserRoles.stream().anyMatch(spvRoles::contains)) {
       isSpv = true;
     }
 
@@ -282,6 +286,14 @@ public class RoleAssignmentValidator {
   }
 
   private void validateRoleRestrictions(List<String> requestingUserRoles, List<String> newRoles) {
+    String configuredStateAdminRoles = ProjectUtil.getConfigValue(JsonKey.STATE_ADMIN_ROLES);
+    List<String> stateAdminRoles = StringUtils.isNotBlank(configuredStateAdminRoles)
+            ? List.of(configuredStateAdminRoles.split(","))
+            : List.of(JsonKey.STATE_ADMIN);
+    if (requestingUserRoles.stream().anyMatch(stateAdminRoles::contains)) {
+      return;
+    }
+
     Map<String, Object> restrictionsConfig = ProjectUtil.loadFilters(JsonKey.ROLE_ASSIGNMENT_RESTRICTIONS);
 
     List<String> restrictedRoles;
